3d593ac47afd2fb900337c519bdc4f900d7448c5
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2020-07-18 02:11:20 -0400

    guild tutorial

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVERTOK = os.getenv('SERVER_ID')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    for server in client.guilds:
+        if server == SERVERTOK:
+            break
+    print(f'{client.user} is connected to the following guild:\n'f'{server.name}(id: {server.id})')
 
 client.run(TOKEN)
